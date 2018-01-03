577d59939b92e5535fe835abff62d735eda64ee4
yunsi-user <yunsi-user@win>
2018-01-03 14:51:25 +0800

    删除无用.java文件
    Change-Id: I328e8c3496715ffe0d0b8d8110eb16102ab2140d
    Signed-off-by: yunsi-user <yunsi-user@win>

--- src/com/jsyunsi/mobile_manager/SMSForward.java
+++
@@ -1,5 +0,0 @@
-package com.jsyunsi.mobile_manager;
-
-public class SMSForward {
-
-}
---
+++ src/com/jsyunsi/mobile_manager/services/SMSForward.java
@@ -0,0 +1,11 @@
+package com.jsyunsi.mobile_manager.services;
+
+/**
+ * 短信转发服务
+ * 
+ * @author Administrator
+ *
+ */
+public class SMSForward {
+
+}
