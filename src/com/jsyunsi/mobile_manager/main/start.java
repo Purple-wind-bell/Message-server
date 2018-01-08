c26874c3a821487a9ef2ae719dbcb43a4f5b484d
yunsi-user <yunsi-user@win>
2018-01-08 15:01:10 +0800

    创建配置文件
    Change-Id: Icda3b432a53f8acb0bbbb09594c37c46c9d66509
    Signed-off-by: yunsi-user <yunsi-user@win>

--- src/com/jsyunsi/mobile_manager/main/start.java
+++ src/com/jsyunsi/mobile_manager/main/start.java
@@ -10,9 +10,10 @@ public class start {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("开始监听注册服务器...");
 		new RegisterServer().start();
-		System.out.println("开始监听消息服务器...");
+		System.out.println("启动注册服务器...");
 		new SMSServer().start();
+		System.out.println("启动消息服务器...");
 	}
+
 }
--- src/com/jsyunsi/mobile_manager/util/Constant.java
+++ src/com/jsyunsi/mobile_manager/util/Constant.java
@@ -1,4 +1,9 @@
-package com.jsyunsi.mobile_manager.config;
+package com.jsyunsi.mobile_manager.util;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.util.Properties;
 
 /**
  * 参数配置
@@ -8,17 +13,35 @@ package com.jsyunsi.mobile_manager.config;
  */
 public final class Constant {
 	/** MySQL数据库连接 */
-	static String mysqlUrl = "jdbc:mysql://localhost:3306/mobile_message";
+	static String mysqlUrl;
 	/** MySQL用户 */
-	static String mysqlUser = "root";
+	static String mysqlUser;
 	/** MySQL用户密码 */
-	static String mysqlPasswd = "123456";
+	static String mysqlPasswd;
 	/** 客户端普通短信接收端口 */
-	static int clientSMSPort = 5700;
+	static int clientSMSPort;
 	/** 服务器普通短信收发端口 */
-	static int serverSMSPort = 5600;
+	static int serverSMSPort;
 	/** 服务器注册登录端口 */
-	static int registerPort = 5650;
+	static int registerPort;
+
+	static {
+		Properties p = new Properties();
+		InputStream in = Constant.class.getClassLoader().getResourceAsStream("mobile.properties");
+		try {
+			p.load(in);
+			in.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		mysqlUrl = p.getProperty("mysqlUrl");
+		mysqlUser = p.getProperty("mysqlUser");
+		mysqlPasswd = p.getProperty("mysqlPasswd");
+		clientSMSPort = Integer.parseInt(p.getProperty("clientSMSPort"));
+		serverSMSPort = Integer.parseInt(p.getProperty("serverSMSPort"));
+		registerPort = Integer.parseInt(p.getProperty("registerPort"));
+	}
 
 	private Constant() {
 	}
